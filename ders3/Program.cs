55c97f8cd436ede6b9428390a4e2ac1c9b9965e0
Emirhan <emircftsrn@gmail.com>
2025-07-10 12:47:01 +0300

    ghgh
    gfg

---
+++ ders3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region if else
+
+            string capital, country;
+
+
+            Console.Write("Ülkeyi giriniz: ");
+            country = Console.ReadLine();
+
+            Console.Write("Başkenti giriniz: ");
+            capital = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            if (capital == "ankara" & country == "türkiye")
+            {
+                Console.WriteLine("Veriler doğrulandı");
+            }
+            else
+            {
+                Console.WriteLine("Hatalı bilgi! ");
+            }
+
+
+            #endregion
+            Console.Read();
+
+        }
+    }
+}
