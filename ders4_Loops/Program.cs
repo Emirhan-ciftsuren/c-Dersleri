47af7712b2a91e295ef9ad692260b987dc849af2
Emirhan <emircftsrn@gmail.com>
2025-07-10 14:50:08 +0300

    bakteri
    saat başı ikiye bölünen bakterinin gün sonundaki durumu.

---
+++ ders4_Loops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders4_Loops
+{  
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region for
+
+            int bacterium=1;
+            for (int i = 1; i <= 24; i++)
+            {
+                bacterium *= 2;
+                Console.WriteLine(i+" Saat sonunda "+bacterium+" bakteri bulunur");
+            }
+            Console.Read();
+            #endregion
+        }
+    }
+}
