ca79e2e59979df8af3dd9b6dadddb9fdebfd3640
Emirhan <emircftsrn@gmail.com>
2025-07-11 17:43:57 +0300

    diziler pratik

---
+++ ders6_diziler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders6_diziler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region diziler
+
+            string[] cars = { "opel", "mercedes", "kia" };
+
+            #endregion
+        }
+    }
+}
