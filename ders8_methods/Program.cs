098966ecc35b34337c492064dcc0a3c2ba789ec0
Emirhan <emircftsrn@gmail.com>
2025-07-16 16:34:18 +0300

    yeni ders pratikleri

---
+++ ders8_methods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders8_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region void metodları
+            //void isimler()
+            //{
+            //    Console.WriteLine("Ahmet");
+            //    Console.WriteLine("Mehmet");
+            //    Console.WriteLine("Ayşe");
+            //    Console.WriteLine("Mahmut");
+
+            //}
+
+            //isimler();
+            //isimler();
+
+            #endregion
+
+            #region geriye döndüren parametreli metodlar
+             int MyMethod(int x, int y)
+            {
+                return x + y;
+            }
+
+          
+            
+                Console.WriteLine(MyMethod(5, 3));
+
+
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
---
+++ ders9_sqlVeritabanı/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ders9_sqlVeritabanı")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ders9_sqlVeritabanı")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("144f458e-d59d-4eed-8226-ffb639cc3c90")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
